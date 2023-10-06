8e170b27edfdcb05f6a1a71e0fb21bad3418b178
shri2222 <sanganwarshri@gmail.com>
2023-10-06 14:03:56 +0530

    bus and train class updated

--- src/main/java/com/example/travelbookingms/BUS.java
+++ src/main/java/com/example/travelbookingms/BUS.java
@@ -1,4 +1,14 @@
 package com.example.travelbookingms;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BUS {
+    @GetMapping("/myBUS")
+    public String getdata(){
+        return "Please book your BUS ticket";
+
+    }
 }
--- src/main/java/com/example/travelbookingms/Train.java
+++ src/main/java/com/example/travelbookingms/Train.java
@@ -1,4 +1,16 @@
 package com.example.travelbookingms;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Train {
+    @GetMapping("/mytrain")
+    public String getdata(){
+        return "Please book your Train ticket";
+
+    }
 }
+
+
